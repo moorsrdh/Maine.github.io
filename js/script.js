099d2f643b9ec1f6135d76d39b499cdeee9d61e0
Amanda Benjamin <moorsrdh@gmail.com>
2026-07-17 14:39:25 -0400

    Add event listener to show answer on button click

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
+const button = document.getElementById("maineButton");
+const answer = document.getElementById("maineAnswer");
 
+button.addEventListener("click", function () {
+  answer.style.display = "block";
+});
 
