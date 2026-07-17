6b0446f892ef1520b7be098af4fc22707e83a373
Amanda Benjamin <moorsrdh@gmail.com>
2026-07-17 15:04:08 -0400

    Fix typo in element ID for answer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("maineButton");
-const answer = document.getElementById("maineAnswer");
+const answer = document.getElementById("maineanswer");
 
 button.addEventListener("click", function () {
   answer.style.display = "block";
